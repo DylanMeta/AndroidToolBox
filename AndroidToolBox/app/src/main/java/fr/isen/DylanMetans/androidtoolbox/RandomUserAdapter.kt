43b3d844030640106e2deda5d56f97795027bab9
DylanMeta <dylanmetans83@gmail.com>
2020-02-10 09:18:44 +0100

    8th

--- AndroidToolBox/app/src/main/java/fr/isen/DylanMetans/androidtoolbox/RandomUserAdapter.kt
+++ AndroidToolBox/app/src/main/java/fr/isen/DylanMetans/androidtoolbox/RandomUserAdapter.kt
@@ -4,8 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.recycler_view_web_cell.*
+import kotlinx.android.synthetic.main.recycler_view_web_cell.view.*
 
-    class UserAdapter(private val users: ArrayList<UserModel>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter(private val users: ArrayList<UserModel>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
             //Add one contact
             val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_web_cell, parent, false)
@@ -24,7 +27,11 @@ import androidx.recyclerview.widget.RecyclerView
         class UserViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
             //Attach current contact to the display list
             fun bind(user: UserModel) {
-                //var personsInfos =
+                view.textPersonName.text = user.name?.first
+                view.textPersonName2.text = user.name?.last
+                view.textViewAdress.text = user.location?.street?.name
+                view.textMail.text = user.email
+                Picasso.get().load(user.picture?.medium).into(view.imageViewAvatar)
             }
         }
 }
